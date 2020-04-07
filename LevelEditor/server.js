7f65b06afc442719f2f1df6d7b66e006c235e3b2
dmarino <d.marino10@uniandes.edu.co>
2020-04-07 12:23:56 -0700

    the back works now, there are some syncronimus functions right now with the files that could block the server... i'll have to do more research and change that in the future... for now i'll leave it as is to work on the front

--- LevelEditor/server.js
+++ LevelEditor/server.js
@@ -29,7 +29,7 @@ class Server {
 
         this.api.get('/api/get_level_list', ( request, response ) => {
             
-            //get the user_id
+            //get the user_id and creates the path
             var user_id = request.body.userid;
             var path = "/data/" + user_id + "/levels";
 
@@ -41,7 +41,7 @@ class Server {
 
         this.api.get('/api/get_object_list', ( request, response ) => {
             
-            //get the user_id
+            //get the user_id and creates the path
             var user_id = request.body.userid;
             var path = "/data/" + user_id + "/objects";
 
@@ -53,19 +53,8 @@ class Server {
 
         this.api.get('/api/load', ( request, response ) => {
 
-            //get the user_id
-            var user_id = request.body.userid;
-            var path = "/data/" + user_id;
-
-            if(request.body.type =="object"){
-
-                path+= "/objects/";
-            }
-            else{
-                path+= "/levels/";
-            }
-            
-            path += request.body.name + ".json";
+            //creates the path
+            var path = this.givePathObject(request.body)
 
             //call method that loads files
             var result = this.loadFile(path,request.body.name);
@@ -75,11 +64,37 @@ class Server {
 
         this.api.post('/api/save', ( request, response ) => {
 
+            //creates the path
+            var path = this.givePathObject(request.body)
+
+            //call method that saves files
+            let result =  this.saveFile(path,request.body.payload, request.body.name);
+            response.send(result);
+
+
         });
 
         this.run()
     }
 
+    //gives the path of an especific objec
+    givePathObject(body){
+
+        var path = "/data/" + body.userid;
+
+        if(body.type =="object"){
+            path+= "/objects/";
+        }
+        else{
+            path+= "/levels/";
+        }
+            
+        path += body.name + ".json";
+        
+        return path;
+    }
+
+    //gives all the elements in a folder in the structure given by the server API documentation
     giveAllByUser(path){
         
         let result = { error: -1 };
@@ -98,35 +113,32 @@ class Server {
 
             //add the data
             result.error = 0;
-            result.payload = this.listFilesInString(files);
+            result.payload = this.listFiles(files);
         }   
         
         return result;
     }
 
-    //list files in a string following api documentation
-    // {"actualname" : "actualname.json"}
-    listFilesInString(files){
+    //returns an array of the names of the files 
+    listFiles(files){
         
         //create a string to store them
-        let string = "[";
+        let payload =[files.length];
         
-        //add them to the string one by one
+        //add them to the array one by one
         for(var i=0;i<files.length;i++){
 
             var name = files[i].split(".json")[0];
-            string =  string.concat('{"' , name , '" : "' , files[i] ,'"}');
-
-            //if there is more add the coma
-            if(i != files.length-1){
-                    string += ","
+            payload[i] = {
+                name: name,
+                filename: files[i]
             }
         };
-        string +="]";
 
-        return string;
+        return payload;
     }
 
+    //loads the file and returns the json answer
     loadFile(path, name){
 
         let result = { error: -1 };
@@ -148,16 +160,38 @@ class Server {
             result.error = 0;
             result.name = name;
             result.bytes = fs.statSync(path)["size"];
-            result.payload = JSON.stringify(object);
+            result.payload = object;
         }   
         
         return result;
     }
 
-    createResponseLoad(object){
-        
+
+    //saves the payload given by parameter in the path given by parameter
+    saveFile(path, payload,name){
+
+        let result = { error: -1 };
+
+        //the actual path
+        path = Path.join( __dirname, path)
+
+
+        try{
+            fs.writeFileSync(path, JSON.stringify(payload))
+
+            result.error=0;
+            result.bytes = fs.statSync(path)["size"];
+            result.name = name;
+
+        }
+        catch{
+            result.error=1;
+        }
+
+        return result;
     }
 
+    //runs the server and waits for requests
     run() {
 
         this.api.set('port', PORT );
