40e21a13bff5787be7537612e684059b8ff4ddd9
dmarino <d.marino10@uniandes.edu.co>
2020-04-06 23:25:40 -0700

    get list of objects and levesl works now

--- LevelEditor/server.js
+++ LevelEditor/server.js
@@ -4,14 +4,18 @@
 import Express from 'express'
 import Path from 'path'
 import HTTP from 'http'
-import FileSystem from 'fs'
 
 
 const PORT = 3000;
 
+//to manage node file system
+const fs = require('fs');
+
 class Server {
 
     constructor() {
+
+
 
         this.api = Express();
         this.api.use( Express.json() )
@@ -20,7 +24,29 @@ class Server {
 
 
         this.api.get('/', ( request, response ) => {
-            response.render('index',{ title:'Greatest Form Demo Ever!'})
+            response.render('index',{ title:'Level editor'})
+        });
+
+        this.api.get('/api/get_level_list', ( request, response ) => {
+            
+            //get the user_id
+            var user_id = request.body.userid;
+            var path = "/data/" + user_id + "/levels";
+
+            var result = this.giveAllByUser(path);
+            response.send(result);
+
+        });
+
+        this.api.get('/api/get_object_list', ( request, response ) => {
+            
+            //get the user_id
+            var user_id = request.body.userid;
+            var path = "/data/" + user_id + "/objects";
+
+            var result = this.giveAllByUser(path);
+            response.send(result);
+
         });
 
         this.api.post('/api', ( request, response ) => {
@@ -77,6 +103,50 @@ class Server {
         return result
     }
 
+
+    giveAllByUser(path){
+        
+        let result = { error: -1 };
+
+        //the actual path
+        path = Path.join( __dirname, path)
+
+        //if there is no dir with that user id then return error
+        if (!fs.existsSync(path)){
+            result.error = 1;
+        }
+        else{
+
+            //get all the files in the directory
+            var files=fs.readdirSync(path);
+
+            //create a string to store them
+            let payload = "[";
+
+            //add them to the string one by one
+            for(var i=0;i<files.length;i++){
+
+                var name = files[i].split(".json")[0];
+
+                payload =  payload.concat('{"' , name , '" : "' , files[i] ,'"}');
+
+                //if there is more add the coma
+                if(i != files.length-1){
+                    payload += ","
+                }
+
+            };
+
+            payload +="]";
+
+            //add the data
+            result.error = 0;
+            result.payload = payload;
+        }   
+        
+        return result;
+    }
+
     run() {
 
         this.api.set('port', PORT );
@@ -87,7 +157,7 @@ class Server {
             let addr = this.listener.address();
             let bind = typeof addr == `string` ? `pipe ${addr}`: `port ${addr.port}`;
 
-            console.log(`Listneing on ${bind}`)
+            console.log(`Listening on ${bind}`)
         });
     }
 }
