2bae10b180e8cfbff461a223e08f0149e87a9c43
Omar Leonardo Pino Reyes <matato_357@hotmail.com>
2020-04-19 17:52:46 -0700

    Starting physics
    physics and world creation

--- LevelEditor/scripts/game/worldController.js
+++ LevelEditor/scripts/game/worldController.js
@@ -1,11 +1,14 @@
 // Copyright (C) 2020 Omar Pino. All rights Reserved
 'use strict';
 
+import Physics from '../lib/Physics.js'
 
+const GRAVITY = Physics.GRAVITY
 export default class worldController{
 
     constructor(){
-
+        this.gVector = Physics.Vec2(0.0,GRAVITY)
+        this.world = Physics.World(this.gVector)
     }
 
     update(){
